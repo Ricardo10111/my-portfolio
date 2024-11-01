78aebacb48dc926d7a9e7197bc97a23a8379ecb5
Ricardo Lopez <albelop1m@gmail.com>
2024-11-01 14:08:31 -0700

    chore: Add PDF to download

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { FiDownload } from 'react-icons/fi'
+import Link from 'next/link'
 
 // components
 import Social from '@/components/Social'
@@ -27,14 +28,20 @@ const Home = () => {
             </p>
             {/* btn and socials */}
             <div className='flex flex-col xl:flex-row items-center gap-8'>
-              <Button
-                variant='outline'
-                size='lg'
-                className='uppercase flex items-center gap-2'
+              <a
+                href='/assets/resume/Ricardo_CV_Eng_v8.pdf'
+                target='_blank' // Abre en una nueva pestaña
+                rel='noopener noreferrer' // Seguridad adicional
               >
-                <span>Download CV</span>
-                <FiDownload className='text-xl' />
-              </Button>
+                <Button
+                  variant='outline'
+                  size='lg'
+                  className='uppercase flex items-center gap-2'
+                >
+                  <span>Download CV</span>
+                  <FiDownload className='text-xl' />
+                </Button>
+              </a>
               <div className='mb-8 xl:mb-0'>
                 <Social
                   containerStyles='flex gap-6'
